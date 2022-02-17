a4b7cc7ca23aaa5f0c2f0816f1cf9b7564d8f127
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-17 11:52:58 -0800

    Add base triangle code

--- src/main.c
+++ src/main.c
@@ -8,8 +8,35 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
 
 // settings
-const int SCR_WIDTH = 1024;
-const int SCR_HEIGHT = 768;
+const int SCR_WIDTH = 1280;
+const int SCR_HEIGHT = 720;
+
+// ========================================
+// GLSL (OpenGL Shading Language)
+// Modern OpenGL requires that we at least set up a vertex and fragment
+// shader if we want to do some rendering
+const char* const VERTEX_SHADER_SOURCE =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   ourColor = aColor;\n"
+    "}\0";
+
+// Fragment shader
+// The fragment shader is all about calculating the color output of your pixels.
+const char* const FRAGMENT_SHADER_SOURCE =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(ourColor, 1.0f);\n"
+    "}\n\0";
+// ========================================
 
 int main()
 {
@@ -43,6 +70,108 @@ int main()
         return -1;
     }
 
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &VERTEX_SHADER_SOURCE, NULL);
+    glCompileShader(vertex_shader);
+    // check for shader compile errors
+    int success = 0;
+    char info_log[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        fputs("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n", stderr);
+        fputs(info_log, stderr);
+    }
+    // fragment shader
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &FRAGMENT_SHADER_SOURCE, NULL);
+    glCompileShader(fragment_shader);
+    // check for shader compile errors
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        fputs("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n", stderr);
+        fputs(info_log, stderr);
+    }
+
+    // link shaders
+    // A shader program object is the final linked version of multiple shaders combined.
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    // check for linking errors
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        fputs("ERROR::SHADER::PROGRAM::LINKING_FAILED\n", stderr);
+        fputs(info_log, stderr);
+    }
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // First 3 are positions; last 3 are colours.
+    // ------------------------------------------------------------------
+    // clang-format off
+    float vertices[] = {
+    0.5F, -0.5F, 0.0F, 1.0F, 0.0F, 0.0F, // bottom right
+    -0.5F, -0.5F, 0.0F, 0.0F, 1.0F, 0.0F, // bottom left
+    0.0F,  0.5F, 0.0F, 0.0F, 0.0F, 1.0F  // top
+    };
+    // clang-format on
+
+    // Vertex Buffer Objects and Vertex Array Object
+    unsigned int vbo = 0;
+    unsigned int vao = 0;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure
+    // vertex attributes(s).
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    // 1) The first parameter specifies which vertex attribute we want to configure.
+    // Remember that we specified the location of the position vertex attribute in the vertex shader
+    // with layout (location = 0). This sets the location of the vertex attribute to 0 and since we
+    // want to pass data to this vertex attribute, we pass in 0. 2) The next argument specifies the
+    // size of the vertex attribute. The vertex attribute is a vec3 so it is composed of 3 values.
+    // 3) The third argument specifies the type of the data which is GL_FLOAT (a vec* in GLSL
+    // consists of floating point values). 4) The next argument specifies if we want the data to be
+    // normalized. If we're inputting integer data types (int, byte) and we've set this to GL_TRUE,
+    // the integer data is normalized to 0 (or -1 for signed data) and 1 when converted to float.
+    // This is not relevant for us so we'll leave this at GL_FALSE.
+    // 5) The fifth argument is known as the stride and tells us the space between consecutive
+    // vertex attribute sets. Since the next set of position data is located exactly 3 times the
+    // size of a float away we specify that value as the stride. Note that since we know that the
+    // array is tightly packed (there is no space between the next vertex attribute value) we
+    // could've also specified the stride as 0 to let OpenGL determine the stride (this only works
+    // when values are tightly packed). 6) The last parameter is of type void* and thus requires
+    // that weird cast. This is the offset of where the position data begins in the buffer. Since
+    // the position data is at the start of the data array this value is just 0.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    // NOLINTNEXTLINE(performance-no-int-to-ptr)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but
+    // this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+    // VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    // as we only have a single shader, we could also just activate our shader once beforehand if we
+    // want to
+    glUseProgram(shader_program);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window)) {
@@ -50,11 +179,20 @@ int main()
         // -----
         process_input(window);
 
+        // render the triangle
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -69,9 +207,6 @@ void process_input(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1);
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        puts("W Pressed");
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
