79bb4451f59a0c675f61202b24f6763a82bb5a2d
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-18 17:48:55 -0800

    Include GLSL in clang-format

--- resources/vertex.glsl
+++ resources/vertex.glsl
@@ -1,13 +1,13 @@
 #version 330 core
 
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 colour;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 colour;
 out vec3 vertex_colour;
 
 uniform float time;
 
 void main()
 {
-   gl_Position = vec4(position, 1.0);
-   vertex_colour = colour * (sin(time) / 2.0 + 0.5);
+    gl_Position = vec4(position, 1.0);
+    vertex_colour = colour * (sin(time) / 2.0 + 0.5);
 }
