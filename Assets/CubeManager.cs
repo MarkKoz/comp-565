6e8609b1eea412c4502fa3f7f05fa5fc3fbfe313
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-03-07 18:17:13 -0800

    Handle button presses

--- Assets/CubeManager.cs
+++ Assets/CubeManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CubeManager : MonoBehaviour
 {
     [CanBeNull] private Camera mainCamera;
+    public UIManager ui;
 
     private void Start()
     {
---
+++ Assets/UIManager.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public enum Texture
+    {
+        StoneBricks01,
+        StoneBricks02,
+        Metal01,
+    }
+
+    public Texture texture = Texture.StoneBricks01;
+    public PrimitiveType primitive = PrimitiveType.Cube;
+
+    public void OnTextureClick(int index)
+    {
+        texture = IntToEnum<Texture>(index);
+    }
+
+    public void OnPrimitiveClick(int index)
+    {
+        primitive = IntToEnum<PrimitiveType>(index);
+    }
+
+    private static T IntToEnum<T>(int index) where T : Enum
+    {
+        if (Enum.IsDefined(typeof(T), index))
+            return (T)Enum.ToObject(typeof(T), index);
+
+        throw new ArgumentOutOfRangeException(nameof(index), $"Invalid index for {nameof(T)}.");
+    }
+}
